cfbe4de301f7b1f66505c697576d429e27ac3710
Guilherme <crocamoguilherme@gmail.com>
2023-11-20 19:44:00 -0300

    Class Position

---
+++ src/application/TestChessMatch.java
@@ -0,0 +1,13 @@
+package application;
+
+import board.Position;
+
+public class TestChessMatch {
+
+	public static void main(String[] args) {
+		
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
+	}
+
+}
